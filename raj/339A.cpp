dfe23497e6d49b5dbb4fa5bb4b0c202c78897f0d
dasagreeva <akashraj5399@gmail.com>
2021-04-12 03:06:17 +0530

    journey started

--- raj/339A.cpp
+++ raj/339A.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main(){
-  cout << "thinker" << endl ;
-  cout << 'a' << endl ;
+  string ste;
+  cin >> ste ;
+  size_t a = count(ste.begin(), ste.end(), '1');
+  size_t b = count(ste.begin(), ste.end(), '2');
+  size_t c = count(ste.begin(), ste.end(), '3');
+
+  ste = "";
+
+  while(a--)
+    ste += "1+" ;
+  while(b--)
+    ste += "2+" ;
+  while(c--)
+    ste += "3+" ;
+  cout << ste.substr(0, ste.size()-1) << endl ;
+
   return 0;
+
 }
